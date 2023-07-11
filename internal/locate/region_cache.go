44955e69dae77d6df933de7ca483904161f00675
you06 <you1474600@gmail.com>
2023-07-11 15:43:57 +0800

    schedule reload when pending or down peers
    Signed-off-by: you06 <you1474600@gmail.com>

--- internal/locate/region_cache.go
+++ internal/locate/region_cache.go
@@ -1134,7 +1134,7 @@ func (c *RegionCache) LocateRegionByID(bo *retry.Backoffer, regionID uint64) (*K
 	c.mu.RUnlock()
 	if r != nil {
 		if r.checkNeedReloadAndMarkUpdated() {
-			lr, err := c.loadRegionByID(bo, regionID)
+			lr, _, err := c.loadRegionByID(bo, regionID)
 			if err != nil {
 				// ignore error and use old region info.
 				logutil.Logger(bo.GetCtx()).Error("load region failure",
@@ -1155,7 +1155,7 @@ func (c *RegionCache) LocateRegionByID(bo *retry.Backoffer, regionID uint64) (*K
 		return loc, nil
 	}
 
-	r, err := c.loadRegionByID(bo, regionID)
+	r, _, err := c.loadRegionByID(bo, regionID)
 	if err != nil {
 		return nil, err
 	}
@@ -1186,7 +1186,7 @@ func (c *RegionCache) scheduleReloadRegion(region *Region) {
 
 func (c *RegionCache) reloadRegion(regionID uint64) {
 	bo := retry.NewNoopBackoff(c.ctx)
-	lr, err := c.loadRegionByID(bo, regionID)
+	lr, downOrPending, err := c.loadRegionByID(bo, regionID)
 	if err != nil {
 		// ignore error and use old region info.
 		logutil.Logger(bo.GetCtx()).Error("load region failure",
@@ -1195,6 +1195,10 @@ func (c *RegionCache) reloadRegion(regionID uint64) {
 			atomic.StoreInt32(&oldRegion.asyncReload, 0)
 		}
 		return
+	}
+	// immediately schedule next reload because there are down or pending peers.
+	if downOrPending {
+		c.scheduleReloadRegion(lr)
 	}
 	c.mu.Lock()
 	c.insertRegionToCache(lr, false)
@@ -1552,7 +1556,7 @@ func (c *RegionCache) loadRegion(bo *retry.Backoffer, key []byte, isEndKey bool)
 }
 
 // loadRegionByID loads region from pd client, and picks the first peer as leader.
-func (c *RegionCache) loadRegionByID(bo *retry.Backoffer, regionID uint64) (*Region, error) {
+func (c *RegionCache) loadRegionByID(bo *retry.Backoffer, regionID uint64) (*Region, bool, error) {
 	ctx := bo.GetCtx()
 	if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
 		span1 := span.Tracer().StartSpan("loadRegionByID", opentracing.ChildOf(span.Context()))
@@ -1564,7 +1568,7 @@ func (c *RegionCache) loadRegionByID(bo *retry.Backoffer, regionID uint64) (*Reg
 		if backoffErr != nil {
 			err := bo.Backoff(retry.BoPDRPC, backoffErr)
 			if err != nil {
-				return nil, errors.WithStack(err)
+				return nil, false, errors.WithStack(err)
 			}
 		}
 		start := time.Now()
@@ -1577,19 +1581,20 @@ func (c *RegionCache) loadRegionByID(bo *retry.Backoffer, regionID uint64) (*Reg
 		}
 		if err != nil {
 			if isDecodeError(err) {
-				return nil, errors.Errorf("failed to decode region range key, regionID: %q, err: %v", regionID, err)
+				return nil, false, errors.Errorf("failed to decode region range key, regionID: %q, err: %v", regionID, err)
 			}
 			backoffErr = errors.Errorf("loadRegion from PD failed, regionID: %v, err: %v", regionID, err)
 			continue
 		}
 		if reg == nil || reg.Meta == nil {
-			return nil, errors.Errorf("region not found for regionID %d", regionID)
+			return nil, false, errors.Errorf("region not found for regionID %d", regionID)
 		}
 		filterUnavailablePeers(reg)
 		if len(reg.Meta.Peers) == 0 {
-			return nil, errors.New("receive Region with no available peer")
+			return nil, false, errors.New("receive Region with no available peer")
 		}
-		return newRegion(bo, c, reg)
+		region, err := newRegion(bo, c, reg)
+		return region, len(reg.DownPeers) > 0 || len(reg.PendingPeers) > 0, err
 	}
 }
 
@@ -1972,7 +1977,7 @@ func (c *RegionCache) UpdateBucketsIfNeeded(regionID RegionVerID, latestBucketsV
 		// TODO(youjiali1995): use singleflight.
 		go func() {
 			bo := retry.NewBackoffer(context.Background(), 20000)
-			new, err := c.loadRegionByID(bo, regionID.id)
+			new, _, err := c.loadRegionByID(bo, regionID.id)
 			if err != nil {
 				logutil.Logger(bo.GetCtx()).Error("failed to update buckets",
 					zap.String("region", regionID.String()), zap.Uint64("bucketsVer", bucketsVer),

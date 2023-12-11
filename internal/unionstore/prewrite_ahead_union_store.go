24ee71301cbdfe57de41b943906e7ae18496e572
you06 <you1474600@gmail.com>
2023-12-11 19:43:01 +0900

    done flush unionstore
    Signed-off-by: you06 <you1474600@gmail.com>

--- internal/unionstore/prewrite_ahead_union_store.go
+++ internal/unionstore/prewrite_ahead_union_store.go
@@ -29,7 +29,7 @@ type PrewriteAheadKVMemDBUnionStore struct {
 	snapshot  uSnapshot
 	memBuffer *MemDB
 	// The memdb which is flushing to stores, should be overwritten by memBuffer when reading.
-	onFlushingMemDB *MemDB
+	onFlushingMemBuffer *MemDB
 	// flush the given memdb to stores, so that snapshot read can see the data.
 	// before the flush is completed, the data will be read from memdb.
 	// to avoid race, DO NOT WRITE the memdb in flush function.
@@ -40,10 +40,10 @@ type PrewriteAheadKVMemDBUnionStore struct {
 // NewPrewriteAheadKVMemDBUnionStore builds a new unionStore with multi memdb.
 func NewPrewriteAheadKVMemDBUnionStore(snapshot uSnapshot, worker int, flushHandle func(*MemDB) error) *PrewriteAheadKVMemDBUnionStore {
 	unistore := &PrewriteAheadKVMemDBUnionStore{
-		snapshot:        snapshot,
-		memBuffer:       newMemDB(),
-		onFlushingMemDB: newMemDB(),
-		flush:           flushHandle,
+		snapshot:            snapshot,
+		memBuffer:           newMemDB(),
+		onFlushingMemBuffer: newMemDB(),
+		flush:               flushHandle,
 	}
 	return unistore
 }
@@ -56,11 +56,13 @@ func (us *PrewriteAheadKVMemDBUnionStore) GetMemBuffer() *MemDB {
 // Get implements the Retriever interface.
 func (us *PrewriteAheadKVMemDBUnionStore) Get(ctx context.Context, k []byte) ([]byte, error) {
 	us.RLock()
-	v, err := us.memBuffer.Get(k)
-	if tikverr.IsErrNotFound(err) {
-		v, err = us.onFlushingMemDB.Get(k)
-	}
+	memBuffer, onFlushingMemBuffer := us.memBuffer, us.onFlushingMemBuffer
 	us.RUnlock()
+
+	v, err := memBuffer.Get(k)
+	if tikverr.IsErrNotFound(err) {
+		v, err = onFlushingMemBuffer.Get(k)
+	}
 	if tikverr.IsErrNotFound(err) {
 		v, err = us.snapshot.Get(ctx, k)
 	}
@@ -73,17 +75,34 @@ func (us *PrewriteAheadKVMemDBUnionStore) Get(ctx context.Context, k []byte) ([]
 	return v, nil
 }
 
+func (us *PrewriteAheadKVMemDBUnionStore) getIts(memdb2it func(*MemDB) (Iterator, error)) (Iterator, Iterator, error) {
+	us.RLock()
+	memdb, onFlushingMemdb := us.memBuffer, us.onFlushingMemBuffer
+	us.RUnlock()
+	memdbIt, err := memdb2it(memdb)
+	if err != nil {
+		return nil, nil, err
+	}
+	onFlushingMemdbIt, err := memdb2it(onFlushingMemdb)
+	if err != nil {
+		return nil, nil, err
+	}
+	return memdbIt, onFlushingMemdbIt, nil
+}
+
 // Iter implements the Retriever interface.
 func (us *PrewriteAheadKVMemDBUnionStore) Iter(k, upperBound []byte) (Iterator, error) {
 	retrieverIt, err := us.snapshot.Iter(k, upperBound)
 	if err != nil {
 		return nil, err
 	}
-
-	its, err := us.getIts(retrieverIt, func(memDB *MemDB) (Iterator, error) {
+	memdbIt, onFlushingMemdbIt, err := us.getIts(func(memDB *MemDB) (Iterator, error) {
 		return memDB.Iter(k, upperBound)
 	})
-	return NewMultiMemDBUnionIter(its, false)
+	if err != nil {
+		return nil, err
+	}
+	return NewMultiMemDBUnionIter([3]Iterator{retrieverIt, onFlushingMemdbIt, memdbIt}, false)
 }
 
 // IterReverse implements the Retriever interface.
@@ -92,24 +111,27 @@ func (us *PrewriteAheadKVMemDBUnionStore) IterReverse(k, lowerBound []byte) (Ite
 	if err != nil {
 		return nil, err
 	}
-	its, err := us.getIts(retrieverIt, func(memDB *MemDB) (Iterator, error) {
+	memdbIt, onFlushingMemdbIt, err := us.getIts(func(memDB *MemDB) (Iterator, error) {
 		return memDB.IterReverse(k, lowerBound)
 	})
-	return NewMultiMemDBUnionIter(its, false)
+	if err != nil {
+		return nil, err
+	}
+	return NewMultiMemDBUnionIter([3]Iterator{retrieverIt, onFlushingMemdbIt, memdbIt}, true)
 }
 
 // MultiMemDBUnionIter is the iterator on an UnionStore.
 type MultiMemDBUnionIter struct {
 	// the index 0 is snapshot iterator, the higher index always overwrite the lower.
-	its     []Iterator
-	valids  []bool
+	its     [3]Iterator
+	valids  [3]bool
 	cur     int
 	isValid bool
 	reverse bool
 }
 
-func NewMultiMemDBUnionIter(its []Iterator, reverse bool) (*MultiMemDBUnionIter, error) {
-	valids := make([]bool, len(its))
+func NewMultiMemDBUnionIter(its [3]Iterator, reverse bool) (*MultiMemDBUnionIter, error) {
+	var valids [3]bool
 	for i := range valids {
 		valids[i] = its[i].Valid()
 	}
@@ -125,59 +147,118 @@ func NewMultiMemDBUnionIter(its []Iterator, reverse bool) (*MultiMemDBUnionIter,
 	return iter, nil
 }
 
+func (iter *MultiMemDBUnionIter) next(i int) error {
+	err := iter.its[i].Next()
+	iter.valids[i] = iter.its[i].Valid()
+	return err
+}
+
+// updateCur2 updates the cur between 2 iterators, return true if the cur non-delete.
+func (iter *MultiMemDBUnionIter) updateCur2(cmp, i, j int) (bool, error) {
+	switch cmp {
+	case 0:
+		if len(iter.its[j].Value()) == 0 {
+			if err := iter.next(i); err != nil {
+				return false, err
+			}
+			if err := iter.next(j); err != nil {
+				return false, err
+			}
+		}
+		if err := iter.next(i); err != nil {
+			return false, err
+		}
+		iter.cur = j
+	case 1:
+		iter.cur = j
+	case -1:
+		iter.cur = i
+	default:
+		panic("unreachable")
+	}
+	return true, nil
+}
+
 func (iter *MultiMemDBUnionIter) updateCur() error {
 	iter.isValid = true
 	for {
-		if iter.cur >= 0 {
-			if iter.reverse {
-				if err := iter.its[iter.cur].Prev(); err != nil {
-					return err
-				}
-			} else {
-				if err := iter.its[iter.cur].Next(); err != nil {
-					return err
-				}
-			}
-		}
-		iter.cur = -1
-		for i := range iter.its {
-			if !iter.valids[i] {
-				continue
-			}
-			if iter.cur == -1 {
-				iter.cur = i
-				continue
-			}
-			cmp := bytes.Compare(iter.its[i].Key(), iter.its[iter.cur].Key())
-			if iter.reverse {
-				cmp = -cmp
-			}
-			if cmp < 0 {
-				iter.cur = i
-			}
-		}
-		if iter.cur == -1 {
-			iter.isValid = false
-			return nil
-		}
-		for i := range iter.its {
-			if i == iter.cur {
-				continue
-			}
-			cmp := bytes.Compare(iter.its[i].Key(), iter.its[iter.cur].Key())
-			if iter.reverse {
-				cmp = -cmp
-			}
-			if cmp == 0 {
-				iter.valids[i] = true
-			} else {
-				iter.valids[i] = false
-			}
-		}
-		if iter.its[iter.cur].Valid() {
+		if !iter.valids[0] && !iter.valids[1] && !iter.valids[2] {
 			break
 		}
-		iter.valids[iter.cur] = false
+
+		// maybe use bit map? because 3 compare result can be saved in one int.
+		var cmps [2]int
+		cmps[0] = bytes.Compare(iter.its[0].Key(), iter.its[1].Key())
+		cmps[1] = bytes.Compare(iter.its[1].Key(), iter.its[2].Key())
+
+		if iter.reverse {
+			for i := range cmps {
+				cmps[i] = -cmps[i]
+			}
+		}
+
+		switch cmps {
+		case [2]int{-1, -1}, [2]int{-1, 0}:
+			// first iter is the smallest.
+			iter.cur = 0
+			break
+		case [2]int{-1, 1}:
+			cmp2 := bytes.Compare(iter.its[0].Key(), iter.its[2].Key())
+			if iter.reverse {
+				cmp2 = -cmp2
+			}
+			stop, err := iter.updateCur2(cmp2, 0, 2)
+			if err != nil {
+				return err
+			}
+			if stop {
+				break
+			}
+		case [2]int{0, -1}:
+			stop, err := iter.updateCur2(0, 0, 1)
+			if err != nil {
+				return err
+			}
+			if stop {
+				break
+			}
+		case [2]int{0, 1}, [2]int{1, 1}:
+			iter.cur = 2
+			break
+		case [2]int{1, 0}:
+			stop, err := iter.updateCur2(0, 1, 2)
+			if err != nil {
+				return err
+			}
+			if stop {
+				break
+			}
+		case [2]int{1, -1}:
+			iter.cur = 1
+			break
+		case [2]int{0, 0}:
+			// need to skip delete record.
+			if len(iter.its[2].Value()) == 0 {
+				if err := iter.next(0); err != nil {
+					return err
+				}
+				if err := iter.next(1); err != nil {
+					return err
+				}
+				if err := iter.next(2); err != nil {
+					return err
+				}
+				continue
+			}
+			if err := iter.next(0); err != nil {
+				return err
+			}
+			if err := iter.next(1); err != nil {
+				return err
+			}
+			iter.cur = 2
+			break
+		}
 	}
 	return nil
 }
@@ -195,6 +276,9 @@ func (iter *MultiMemDBUnionIter) Value() []byte {
 }
 
 func (iter *MultiMemDBUnionIter) Next() error {
+	if err := iter.its[iter.cur].Next(); err != nil {
+		return err
+	}
 	return iter.updateCur()
 }
 

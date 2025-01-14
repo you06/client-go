e5110698e40b6955095a6bac58a54448f91c81ad
you06 <you1474600@gmail.com>
2025-01-14 15:02:29 +0900

    fix test that commit empty txn
    Signed-off-by: you06 <you1474600@gmail.com>

--- txnkv/transaction/2pc.go
+++ txnkv/transaction/2pc.go
@@ -2408,7 +2408,7 @@ func (c *twoPhaseCommitter) getCommitInfo() *util.CommitInfo {
 		CommitTS:    atomic.LoadUint64(&c.commitTS),
 		MutationLen: c.mutations.Len(),
 		TxnSize:     c.txnSize,
-		Primary:     c.primary(),
+		Primary:     c.primaryKey,
 	}
 }
 
